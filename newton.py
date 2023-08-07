d56571c7f08eaf4076bc64a7760371f1b60c26e4
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-08-07 16:07:13 +0200

    newton method for nonlinear system of equations

--- newton.py
+++ newton.py
@@ -1,7 +1,15 @@
+"""
+newton method for nonlinear system of equations
+"""
 import numpy as np
-from scipy.optimize import root
+import matplotlib.pyplot as plt
+from scipy.optimize import fsolve, root
 
-def newton(f, start, tol, args=()):
+#=============================================================
+# Newron method implementation
+#=============================================================
+
+def newton(f, start, tol, args=(), dense_output=False, max_it=1000):
     '''
     Generalizzation of newton method for n equations
     
@@ -10,16 +18,23 @@ def newton(f, start, tol, args=()):
     f : callable
         A vector function to find a root of.
     start : ndarray
-        Initial guess.
+        Initial guess. The method is very sensitive to this
+        must be chosen carefully
     tol :float, optional, default 1e-8
         required tollerance
     args : tuple, optional
         Extra arguments passed to f
+    dense_output : bool, optional
+        true for full and number of iteration
+    max_it : int, optional, default 1000
+        after max_it iteration the code stop raising an exception
     
     Return
     ------
     x0 : 1darray
         solution of the system
+        if dense_outupt=True all iteration are returned
+        in a matrix called X and also the number of iteration
     '''
     
     # initial guess
@@ -30,14 +45,17 @@ def newton(f, start, tol, args=()):
     df = np.zeros((nd, nd))
     h  = 1e-8
     s  = np.zeros(nd)
+    #for full output
+    X = []
+    if dense_output : X.append(x0)
     # count
-    #n_iter = 0
+    n_iter = 0
     
     while True:
         
-        # compute jacobian
-        for i in range(nd):
-            for j in range(nd):
+        # compute jacobian using symmetric derivative
+        for i in range(nd):       # loop over functions
+            for j in range(nd):   # loop over variables
                 s[j] = 1
                 xr, xl = x0 + h*s, x0 - h*s
                 df[i, j] = (f(xr, *args) - f(xl, *args) )[i]/(2*h)
@@ -47,29 +65,67 @@ def newton(f, start, tol, args=()):
         delta = np.linalg.solve(df, f0)
         x0 = x0 - delta
         f0 = f(x0, *args)
-        #n_iter += 1        
+        
+        if dense_output:
+            n_iter += 1        
+            X.append(x0)
         
         # stop condition
-        if all(abs(f0) < tol) :
-           break
-           
-    return x0
-        
-def system(V, a):
+        if all(abs(f0) < tol):
+            break
+        # check iterations
+        if n_iter > max_it :
+            err_msg = 'too many iteration, failure to converge, change initial guess'
+            raise Exception(err_msg) 
+    
+    if dense_output:
+        return np.array(X), n_iter       
+    else:
+        return x0
+
+#=============================================================
+# System to solve
+#=============================================================        
+
+def system(V):
     x1, x2 = V
     
-    r1 = x1**2 + x2**2 - a
-    r2 = x2 - x1**2 + x1/2
+    r1 = x1**2 + x2**2 - 1#x3
+    r2 = x2 - x1**2 + x1/2 #+ x3/5
+    #r3 = x3**2 + 5*x2 - 7
     
-    R = np.array([r1, r2])
+    R = np.array([r1, r2])#, r3])
     return R
 
-init = np.array([0.2, 0])
+#=============================================================
+# Solution and copare with scipy
+#=============================================================    
+
+init = np.array([0.2, 0.0])
 tol = 1e-12
+sol, n_iter = newton(system, init, tol=tol, dense_output=True)
+xs, ys = sol.T
+print("Solution with newton: ", *sol[-1], "in", n_iter, "iterations")
 
-sol = newton(system, init, tol, args=(1, ))
-print("Solution with newton: ", *sol)
-
-start = (0.2, 0.0)
-sol = root(system, start, method='hybr', args=(1, ))
+sol = root(system, init, method='hybr', tol=tol)
 print("Solution with root:   ", *sol.x)
+
+sol = fsolve(system , init, xtol=tol)
+print("Solution with fsolve: ", *sol)
+
+#=============================================================
+# Plot
+#=============================================================    
+
+t = np.linspace(0, 2*np.pi, 1000)
+z = np.linspace(-0.8, 1.5, 1000)
+plt.figure(1)
+plt.grid()
+plt.plot(np.cos(t), np.sin(t), 'r', label='first equation')
+plt.plot(z, z**2- z/2, 'b', label='second equation')
+plt.plot(xs, ys, 'k', label='evolution of solution')
+plt.legend(loc='best')
+plt.show()
+
+
+
